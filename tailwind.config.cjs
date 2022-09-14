a53252064f528e99f47d4dc400e517750e721a7d
Rafael Pires <rafaelbrpires@gmail.com>
2022-09-14 14:26:00 -0300

    landing page web

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), linear-gradient(89.86deg, #9572FC 13.08%, #43E7AD 63.94%, #E1D55D 4.57%)',
+        'game-gradient': ' linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+        
+      },
+    },
+  },
+  plugins: [],
+}
